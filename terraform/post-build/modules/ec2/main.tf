0e0cbce47ae086d0f1204b2c76834976da2509a8
shivani kumar <shivanikumar@box.com>
2026-04-02 13:40:57 -0500

    Made snapshot and EC2 volume size the same i.e. 30

--- terraform/post-build/modules/ec2/main.tf
+++ terraform/post-build/modules/ec2/main.tf
@@ -111,7 +111,7 @@ resource "aws_instance" "this" {
   associate_public_ip_address = var.associate_public_ip
   
   root_block_device {
-    volume_size = 20
+    volume_size = 30
     volume_type = "gp3"
   }
 
